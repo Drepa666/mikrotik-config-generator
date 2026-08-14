d5de09d8f9b9599cd67e7180892bdd5f924a2276
Bondarenko <@email.com>
2026-08-15 00:32:52 +0300

    chore: видалено зайві backup, patch та temp файли

--- check_line825.py
+++
@@ -1,6 +0,0 @@
-with open('terminal.js', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-print('=== Рядки 820-835 ===')
-for i, l in enumerate(lines[819:835], 820):
-    print(f'{i}: {l.rstrip()}')

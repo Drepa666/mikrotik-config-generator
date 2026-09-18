7fca31180648b52ff3f12ff6bbd0892ae2de554b
GitHub Action <action@github.com>
2026-09-18 19:15:43 +0000

    ci: CACHE_VERSION auto-update [skip ci]

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    ============================================================ */
 'use strict';
 
-const CACHE_NAME = 'mt-config-v2655';
+const CACHE_NAME = 'mt-config-v88';
 
 const API_HOSTS = [
   'api.openai.com',
